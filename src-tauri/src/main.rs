afe28c508b559071ba4b1bee854f2212aeb738de
万福熊 <wanfuxiong@wanfuxiong.com>
2023-03-05 15:28:21 +0800

    update

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayMenu, SystemTrayMenuItem, SystemTrayEvent};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,20 +10,32 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
-    use tauri::Manager;
-    // use tauri::SystemTray;
-    // let tray = SystemTray::new();
+    // let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    // let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+    // let tray_menu = SystemTrayMenu::new()
+    //     .add_item(quit)
+    //     .add_native_item(SystemTrayMenuItem::Separator)
+    //     .add_item(hide);
+    // let tray = SystemTray::new().with_menu(tray_menu);
     tauri::Builder::default()
+        // .system_tray(tray)
         .setup(|app| {
+            let window = app.get_window("main").unwrap();
             #[cfg(debug_assertions)] // only include this code on debug builds
             {
-            let window = app.get_window("main").unwrap();
                 window.open_devtools();
                 window.close_devtools();
             }
+
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .build(tauri::generate_context!())
+        .expect("运行Tauri程序出错")
+        .run(|_app_handle, event| match event {
+            tauri::RunEvent::ExitRequested { api, .. } => {
+                api.prevent_exit();
+            }
+            _ => {}
+        });
 }
